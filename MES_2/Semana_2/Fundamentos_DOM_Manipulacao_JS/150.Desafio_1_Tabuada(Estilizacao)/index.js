a9b7590adfc0cbf30da382d54232f1d04f42b6ab
Vinicius-Coradini <vinicius.coradini@outlook.com>
2024-05-17 18:33:44 -0300

    atualizando estudos

---
+++ MES_2/Semana_2/Fundamentos_DOM_Manipulacao_JS/150.Desafio_1_Tabuada(Estilizacao)/index.js
@@ -0,0 +1,10 @@
+function tabuada() {
+    var numberOne = parseInt(window.document.getElementById('firstInput'));
+    var numberTwo = parseInt(window.document.getElementById('secondInput')); 
+    
+
+    var result= numberOne + numberTwo
+
+
+    console.log('result')
+}
