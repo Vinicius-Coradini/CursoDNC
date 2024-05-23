e1f3380cc43d25990ca6de287636ce265bcbffb6
Vinicius-Coradini <vinicius.coradini@outlook.com>
2024-05-23 15:28:52 -0300

    Interação e estilização

--- MES_2/Semana_2/Fundamentos_DOM_Manipulacao_JS/150.Desafio_1_Tabuada(Estilizacao)/index.js
+++ MES_2/Semana_2/Fundamentos_DOM_Manipulacao_JS/150.Desafio_1_Tabuada(Estilizacao)/index.js
@@ -1,10 +1,29 @@
-function tabuada() {
-    var numberOne = parseInt(window.document.getElementById('firstInput'));
-    var numberTwo = parseInt(window.document.getElementById('secondInput')); 
-    
-
-    var result= numberOne + numberTwo
 
 
-    console.log('result')
+function multiplicationTable() {
+
+     
+    const numberOne = parseInt(window.document.getElementById('firstInput').value);
+    const numberTwo = parseInt(window.document.getElementById('secondInput').value);
+    const tabuada = window.document.getElementById('tabuada')
+
+    let result = ''
+
+    if (isNaN(numberOne) || isNaN(numberTwo)) {
+        alert('Digite os números nos campos')
+        return
+    }
+
+    for (let i = 0; i <= numberTwo; i++) {
+        title = `A tabuada do ${numberOne} é:`
+        document.querySelector('#title').innerHTML = title
+
+        result += `${i} X ${numberOne} = ${i * numberOne}<br>`
+    }
+    document.querySelector('#multiplicationTable').innerHTML = result
+
+    tabuada.style.display = 'block'
+
+
 }
+
