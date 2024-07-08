b47c9dc4f9d5794425608a9a958d62e2df7c4288
Vinicius-Coradini <vinicius.coradini@outlook.com>
2024-07-08 13:46:37 -0300

    Finalização aula 205

--- MES_2/Semana_2/4.Consumindo API de Previsão do tempo/Processando_Dados_API/index.js
+++ MES_2/Semana_2/4.Consumindo API de Previsão do tempo/Processando_Dados_API/index.js
@@ -1,13 +1,36 @@
-function buscarCep() {
+async function buscarCep() {
     var cep = document.getElementById('cep').value;
 
-    fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => {
-            return response.json()
-        }).then(response => {
-            console.log(response)
-        }).catch(error => {
-            console.log(erro)
-        })
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+
+        document.getElementById('rua').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('cidade').value = data.localidade;
+
+
+    } catch (error) {
+        alert(error);
+    }
 }
+
+async function previsao() {
+    const long = document.getElementById('longitude').value;
+    const lati = document.getElementById('latitude').value;
+
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&hourly=temperature_2m`);
+        const data = await response.json();
+
+        document.getElementById('resposta').innerHTML = "";
+        for (let i = 0; i < data.hourly.temperature_2m.length; i++) {
+            document.getElementById('resposta').innerHTML += `<div>${data.hourly.time[i]} - ${data.hourly.temperature_2m[i]}°C </div>`;
+        }
+    }
+    catch (error) {
+        alert(error.message);
+    }
+}
+
 
